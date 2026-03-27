3c4c986e3f13d903df73a62454e7f69457766ecf
Toni Fey <ToniFey@protonmail.com>
2026-03-27 08:02:42 +0100

    Remove scaffold background color from MyApp theme

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        scaffoldBackgroundColor: Colors.white,
       ),
       home: const MyHomePage(title: 'nursery'),
     );
